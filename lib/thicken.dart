e4a7381323f27e9837f1693c4ea1de3b43641ea5
Nialixus <nialixus@gmail.com>
2024-10-06 19:55:06 +0700

    [dev] Experimenting

--- lib/thicken.dart
+++ lib/thicken.dart
@@ -1,7 +1,33 @@
 library thicken;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class Thicken extends StatelessWidget {
+  const Thicken({
+    super.key,
+    this.thickness = 0.15,
+    required this.child,
+  });
+  final double thickness;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          for (int x = 0; x < 3; x++)
+            for (int y = 0; y < 3; y++)
+              Transform.translate(
+                offset: Offset(
+                  [-1, -0.5, 0, 0.5, 1][x] * thickness,
+                  [-1, -0.5, 0, 0.5, 1][y] * thickness,
+                ),
+                child: Center(child: child),
+              ),
+        ],
+      ),
+    );
+  }
 }
